ada501f6810bfe2cd57fc4ed7694dab080b146f6
Rawand-abu-shammala <rwndsami@gmail.com>
2026-05-11 18:40:07 +0300

    fix: fix ESLint unused vars error in classNames

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -7,7 +7,7 @@ export const classNames = (...args: ClassValue[]): string => {
       if (typeof arg === "string") return [arg];
       if (typeof arg === "object") {
         return Object.entries(arg)
-          .filter(([_, value]) => Boolean(value))
+          .filter(([, value]) => Boolean(value))
           .map(([key]) => key);
       }
       return [];
