d3620e066eb1277fb7a04307231ecc270d2af652
Rawand-abu-shammala <rwndsami@gmail.com>
2026-05-11 18:31:41 +0300

    fix: resolve ESLint config and ensure lint:ts works

--- src/pages/Home/ui/Homepage.tsx
+++ src/pages/Home/ui/Homepage.tsx
@@ -4,10 +4,12 @@ import { Header } from "@/widgets/Header";
 import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
+
   return (
-    <div className={styles.pageWrapper}>
+    <div className={styles.pageWrapper}> 
       <Header />
       <main className={styles.content}>1</main>
+      
       <Footer />
     </div>
   );
